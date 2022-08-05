8f86a5a188d62914589b438e23e1b4feffd1ce0f
Zach P <zpetty33@gmail.com>
2022-08-04 20:27:34 -0700

    Merge Master branch updates into Experimental Branch (#84)
    * Update CHANGELOG.md
    
    * Fix Weapon SetClipErrors (#76)
    
    * fix: result isn't used in this context.
    
    * fix: the docs say ply, we use ply.
    
    * perf: we don't need to call that twice.
    
    * fix: printname isn't a name, it's class. localise class.
    
    * fix: fix weapon error if not given.
    
    replace printname with class.
    only call getweapon if we can't get it through give.
    
    * fix: Make sure SetClip1 and SetClip2 actually exist before we call them.
    
    * WTF (#63)
    
    * Update CHANGELOG.md
    
    * Update ulib.build
    
    * Update addon.txt
    
    * Update defines.lua
    
    Co-authored-by: Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
    Co-authored-by: Deyvan <51455765+Deyvan@users.noreply.github.com>

--- lua/ulib/shared/defines.lua
+++ lua/ulib/shared/defines.lua
@@ -7,7 +7,7 @@
 ULib = ULib or {}
 
 ULib.RELEASE = false -- Don't access these two directly, use ULib.pluginVersionStr("ULib")
-ULib.VERSION = 2.70
+ULib.VERSION = 2.71
 ULib.AUTOMATIC_UPDATE_CHECKS = true
 
 ULib.ACCESS_ALL = "user"
