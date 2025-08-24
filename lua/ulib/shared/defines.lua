b7e2b7727ce2a5f46fde3222e07db3996da09343
Malivil <Malivil@users.noreply.github.com>
2025-08-23 21:47:52 -0400

    Replace replicated convar broadcast on join with a compressed and chunked string transmit (#104)

--- lua/ulib/shared/defines.lua
+++ lua/ulib/shared/defines.lua
@@ -466,5 +466,7 @@ if SERVER then
 	util.AddNetworkString( "URPC" )
 	util.AddNetworkString( "tsayc" )
 	util.AddNetworkString( "ulib_repWriteCvar" )
+	util.AddNetworkString( "ulib_repWriteCvarBatch_Part" )
+	util.AddNetworkString( "ulib_repWriteCvarBatch_Complete" )
 	util.AddNetworkString( "ulib_repChangeCvar" )
 end
